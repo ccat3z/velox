cc7d0e6a2386f11b8546f37997f3544a0659e8b6
Lingfeng Zhang <zhanglingfeng04@meituan.com>
2025-02-21 11:26:09 +0800

    Force use indices-based column mapping for old orc

--- velox/dwio/dwrf/reader/DwrfReader.cpp
+++ velox/dwio/dwrf/reader/DwrfReader.cpp
@@ -797,7 +797,8 @@ DwrfReader::DwrfReader(
   // code. So we rename column names in the file schema to match table schema.
   // We test the options to have 'fileSchema' (actually table schema) as most
   // of the unit tests fail to provide it.
-  if ((!readerBase_->readerOptions().useColumnNamesForColumnMapping()) &&
+  if ((!readerBase_->readerOptions().useColumnNamesForColumnMapping() ||
+       readerBase_->isOldSchema()) &&
       (readerBase_->readerOptions().fileSchema() != nullptr)) {
     updateColumnNamesFromTableSchema();
   }
@@ -806,8 +807,11 @@ DwrfReader::DwrfReader(
 void DwrfReader::updateColumnNamesFromTableSchema() {
   const auto& tableSchema = readerBase_->readerOptions().fileSchema();
   const auto& fileSchema = readerBase_->schema();
+
+  auto updateRecursive =
+      !readerBase_->readerOptions().useNestedColumnNamesForColumnMapping();
   readerBase_->setSchema(std::dynamic_pointer_cast<const RowType>(
-      updateColumnNames(fileSchema, tableSchema)));
+      updateColumnNames(fileSchema, tableSchema, updateRecursive)));
 }
 
 std::unique_ptr<StripeInformation> DwrfReader::getStripe(
